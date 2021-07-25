c27405baf9fd1f14d0e6aea9c06b8db61bea5efe
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-07-25 21:50:49 +0300

    Добавьте файлы проекта.

---
+++ Homework_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //вывести массив в обратном порядке
+      int[] array = Enumerable.Range(0, 5).ToArray();
+      Console.WriteLine("Массив в обычном порядке: ");
+      for (int i = 0; i < array.Length; i++)
+      {
+        Console.WriteLine((i + 1) + "-й элемент массива: " + array[i]);
+      }
+      Console.WriteLine("Массив в обратном порядке: ");
+      for (int i = array.Length-1; i >= 0 ; i--)
+      {
+        Console.WriteLine((i + 1) + "-й элемент массива: " + array[i]);
+      }
+      Console.ReadLine();
+
+    }
+  }
+}
